d2a5e13fec36f37b8e7f1d0ed4ac9f5d41a1f876
aurorabora <borabora.choi@gmail.com>
2018-05-02 18:36:24 -0700

    made a todo list and added form inputs using materialize and react

--- src/components/app.js
+++ src/components/app.js
@@ -1,16 +1,51 @@
 import 'materialize-css/dist/css/materialize.min.css';
-import React from 'react';
+import React, { Component } from 'react';
 import '../assets/css/app.css';
-import List from './list.js'
+import AddItem from './add-items';
+import List from './list.js';
+import listData from '../helpers/list_data';
+
+// only App can change list data deletion/ update/ " the thing that owns the data controls the data "
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            listData: []
+        };
+    }
+    // after component is mounted it will re render from this 
+    componentDidMount(){
+        this.getListData();
+    }
+
+    addItem(item){
+        this.setState({
+            listData: [item, ...this.state.listData]
+        });
+    }
+
+    getListData(){
+        setTimeout(() => {
+            this.setState({ listData: listData });   
+        }, 500);  
+    }
 
 
-const App = () => (
-    <div>
-        <div className="container" className="card-panel purple lighten-1">          
-            <h1 className="center">To Do List</h1>
-            <List/>
-        </div>
-    </div>
-);
+    render() {
+        return (
+            <div>
+                <div className="container" className="card-panel purple lighten-1">
+                    <h1 className="center">To Do List</h1>
+                    <AddItem add={this.addItem.bind(this)}/>
+                    <List data={this.state.listData}/>
+                </div>
+            </div>
+        );
+
+    }
+}
+
 
 export default App;
--- src/components/list.js
+++ src/components/list.js
@@ -2,10 +2,13 @@ import React from 'react';
 import listData from '../helpers/list_data';
 
 export default props => {
+    if(!props.data.length){
+        return <h1 className="center white-text"> No To Do Items </h1>
+    }
     
-    console.log('List Data:', listData);
+    console.log('List Data:', props.data);
 
-    const listElements =listData.map((item, index) =>{  //always dom create through maps
+    const listElements =props.data.map((item, index) =>{  //always dom create through maps
             return <li className="collection-item">{item.title}</li>
     });
 
@@ -13,7 +16,7 @@ export default props => {
         <div>
             <h3> To Do </h3>
             <ul className="collection">
-            {listElements}
+                {listElements}
             </ul>
         </div>
     )  
